fa513a45b5d181fe74e5c51d27dd789beff156b4
Donghun kim <kisses217@gmail.com>
2020-11-04 14:41:14 +0900

    :memo: calenarList 추가
    calendarList 추가

--- server/src/controllers/calendar/calendarList.js
+++ server/src/controllers/calendar/calendarList.js
@@ -1,23 +1,27 @@
 const { google } = require("googleapis");
-const scope = "https://www.googleapis.com/auth/calendar.readonly";
 const FAIL = "calendar_invite_fail";
 const SUCCESS = "calendar_invite_success";
+const getAuth = require("./getTk");
 
 module.exports = {
   get: (cohort) => {
-    const calendar = google.calendar({ version: "v3", auth: res });
-    calendar.calendarList.list(
-      {
-        scope: `${scope}/${cohort}`,
-        minAccessRole: "reader",
-      },
-      (err, res) => {
-        if (err) {
-          console.log(err);
-          resolve(FAIL);
-        }
-        resolve(SUCCESS);
-      }
-    );
+    const makeCohort = cohort.replace("Full", "Full Time"); // Full immersive 20기 >> Full Time immersive 20기
+    console.log(makeCohort);
+    return new Promise((resolve, reject) => {
+      getAuth.get().then((res) => {
+        const calendar = google.calendar({ version: "v3", auth: res });
+        calendar.calendarList.list({}, (err, res) => {
+          if (err) {
+            console.log(err);
+            // resolve(FAIL);
+          }
+          res.data.items.map((calendar) => {
+            if (calendar.summary === makeCohort) {
+              resolve(calendar.id);
+            }
+          });
+        });
+      });
+    });
   },
 };
