29e2694faf9bb45dca68bd1d9550de9bc3978f0b
Macintosh HD <sokim1616@gmail.com>
2020-11-10 14:58:29 +0900

    :art: Modified index files

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import rootReducer from './modules/reducer/index';
 
+
 const store = createStore(rootReducer);
 
 ReactDOM.render(
--- client/src/modules/reducer/index.ts
+++ client/src/modules/reducer/index.ts
@@ -1,10 +1,7 @@
-
 import {combineReducers} from 'redux'
 import changeCohortState from '../actions/changeCohortState'
-
 const rootReducer = combineReducers({
     changeCohortState,
 })
-
 export default rootReducer;
 export type RootState = ReturnType<typeof rootReducer>
