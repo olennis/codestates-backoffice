e4108240127ae19eebd0ea8ea91323dea5cd175d
Donghun kim <kisses217@gmail.com>
2020-11-12 15:19:03 +0900

    :books: docs: index 추가

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Page from './components/page/Page';
 
 ReactDOM.render(
   <div>
-
-    <App />
+    <Page />
   </div>
   
   ,
