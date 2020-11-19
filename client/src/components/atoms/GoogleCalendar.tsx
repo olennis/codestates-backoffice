8e0d80603a518f4cb6e45739ba2a7d9e25ae7f3b
김현규 <46562138+codeFabian@users.noreply.github.com>
2020-11-19 16:49:29 +0900

    :rocket: [client] 툴 별 체크 이벤트 구현 중 (#46)

---
+++ client/src/components/atoms/GoogleCalendar.tsx
@@ -0,0 +1,13 @@
+// @flow 
+import * as React from 'react';
+import { CheckAtoms } from "../atoms/CheckAtoms"
+type Props = {
+
+};
+export const GoogleCalendar = (props: Props) => {
+    return (
+        <span>
+            <CheckAtoms /> 구글캘린더
+        </span>
+    );
+};
---
+++ client/src/components/atoms/Slack.tsx
@@ -0,0 +1,14 @@
+// @flow 
+import * as React from 'react';
+import { CheckAtoms } from "./CheckAtoms"
+
+type Props = {
+
+};
+export const Slack = (props: Props) => {
+    return (
+        <span>
+            <CheckAtoms /> 슬랙
+        </span>
+    );
+};
