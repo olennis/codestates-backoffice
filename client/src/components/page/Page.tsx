eb6134797a97b15393c9c0a51f1c27a598daab08
codeFabian <yolo.fabian.k@gmail.com>
2020-11-23 12:22:47 +0900

    page 받은내용 싱크

--- client/src/components/page/Page.tsx
+++ client/src/components/page/Page.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-use-before-define */
-
 import React, { useState } from 'react';
 import { SearchCohort } from '../templates/SearchCohort'
 import { ToolTab } from '../templates/ToolTab'
@@ -9,11 +8,9 @@ import { StudentList } from '../templates/StudentList'
 // type Checkbok = {
 //   check: boolean
 // }
-
 // const defaultCheck: Checkbok = {
 //   check: false
 // }
-
 
 function Page() {
   const [cohort, setCohort] = useState('')
@@ -26,21 +23,13 @@ function Page() {
   const [slackCheck, setSlackCheck] = useState(false) // * 슬랙팀 체크박스 상태관리
   const [calendar, setCalendar] = useState(false) // * 캘린더 체크박스 상태관리
 
-
-
   return (
     <div>
       <b>나는 페이지</b>
-
-
       <SearchCohort cohort={cohort} setCohort={setCohort} setNth={setNth} setName={setName} name={name} nth={nth} setData={setData} ></SearchCohort>
-
       <ToolTab setTab={setTab} setGitCheck={setGitCheck} gitCheck={gitCheck} />
       <StudentList data={data} setStudentData={setStudentData} studentData={studentData} tab={tab} gitCheck={gitCheck}></StudentList>
-
-
     </div>
   );
 }
-
 export default Page;
