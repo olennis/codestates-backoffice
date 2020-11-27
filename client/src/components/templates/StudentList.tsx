718a552adeaa05df70625e7562fb14b6e82de744
Donghun kim <kisses217@gmail.com>
2020-11-27 11:36:25 +0900

    :fire: 주석 제거
    studentList 주석 제거

--- client/src/components/templates/StudentList.tsx
+++ client/src/components/templates/StudentList.tsx
@@ -22,31 +22,7 @@ export const StudentList = (props: Props) => {
         }))
     
     },[])
-    /**
-     * 1. 맨 처음 데이터를 받을 때부터 boolean 값 추가(checked 참조값) O
-     * 2. all 체크박스를 클릭하면 > 상태값 변경(e.target.checked)
-     * 3. 개별 데이터 체크박스는 2번의 상태값 참조
-     */
-    // 11.23 서버 닫힘
-    // if props.data에 체크밸류값이 있다면 > 1번 성공
-    // const sendData = (e: any, index: number, data:any) => {
-    //     if (e.target.checked) {
-    //         console.log('체크 상태:', e.target.checked)
-    //         props.studentData.push(props.data[index])
-    //         console.log('전달 데이터:', props.studentData)
-    //     }
-    //     else if (!e.target.checked) {
-    //         for (let i = 0; i < props.studentData.length; i += 1) {
-    //             if (JSON.stringify(props.studentData[i]) === JSON.stringify(props.data[index])) {
-    //                 console.log('체크 상태:', e.target.checked)
-    //                 props.studentData.splice(i, 1)
-    //                 console.log('전달 데이터:', props.studentData)
-    //             }
-    //         }
-    //     }
-
-    //     return props.studentData
-    // }
+    
     const chooseAll = (e:any) => {
         let checked = e.target.checked 
         props.setData(props.data.map((student:any)=>{
