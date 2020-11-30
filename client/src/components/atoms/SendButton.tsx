e12e42a92a38f2b70716e1bc2cd1bd4a87cf9fca
codeFabian <yolo.fabian.k@gmail.com>
2020-11-30 16:59:55 +0900

    :white_check_mark: [Client] Post Tool 버튼성공

--- client/src/components/atoms/SendButton.tsx
+++ client/src/components/atoms/SendButton.tsx
@@ -1,45 +1,45 @@
 // @flow 
 import React from 'react';
-import axios  from 'axios'
+import axios from 'axios'
 type Props = {
-    data : any
-    cohort : any
-    nth : any
-    gitCheck : any
-    slackCheck : any
-    calendar : any
+    data: any
+    cohort: any
+    nth: any
+    gitCheck: any
+    slackCheck: any
+    calendar: any
 };
 export const SendButton = (props: Props) => {
     const sendData = () => {
-        
-        console.log(props.data,'버튼 클릭!')
-        const people = props.data.reduce((arr:any,student:any)=>{
-            if(student.checkValue){
+
+        console.log(props.data, '버튼 클릭!')
+        const people = props.data.reduce((arr: any, student: any) => {
+            if (student.checkValue) {
                 arr.push(student.id)
             }
             return arr
-        },[])
+        }, [])
         const sendingData = {
-            people : people,
-            cohort : `${props.cohort} ${props.nth}기`,
-            github : props.gitCheck,
-            slack : props.slackCheck,
-            calendar : props.calendar
+            people: people,
+            cohort: `${props.cohort} ${props.nth}기`,
+            github: props.gitCheck,
+            slack: props.slackCheck,
+            calendar: props.calendar
         }
-        
-        console.log(sendingData,'sendingData')
-        axios.post(`https://52a973cf52c1.ngrok.io/tool/moveCohort`,sendingData)
 
-            .then((res)=>{
-                console.log(res,'post요청 응답')
+        console.log(sendingData, 'sendingData')
+        axios.post(`https://52a973cf52c1.ngrok.io/tool/moveCohort`, sendingData)
+
+            .then((res) => {
+                console.log(res, 'post요청 응답')
             })
-            .catch((err)=>{
+            .catch((err) => {
                 console.log(err)
             })
-    
+    }
     return (
         <span>
-            <button onClick={()=>{sendData()}}>가자가자가자가자가자!</button>
+            <button onClick={() => { sendData() }}>가자가자가자가자가자!</button>
         </span>
     );
 };
