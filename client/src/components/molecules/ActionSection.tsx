e2928429325275f116ce07e77343714f1a3adc50
codeFabian <yolo.fabian.k@gmail.com>
2020-12-03 15:28:56 +0900

    코드 맞춤, 자리수정

--- client/src/components/molecules/ActionSection.tsx
+++ client/src/components/molecules/ActionSection.tsx
@@ -1,34 +1,34 @@
 // @flow 
 import React from 'react'
 import { SelectAction } from "../atoms/SelectAction"
-import { SendButton } from "../atoms/SendButton" 
+import { SendButton } from "../atoms/SendButton"
 
 type Props = {
     action: any
     setAction: any
-    data : any
+    data: any
     //객체 테스트
-    cohort : any
-    nth : any
-    gitCheck : any
-    slackCheck : any
-    calendar : any
+    cohort: any
+    nth: any
+    gitCheck: any
+    slackCheck: any
+    calendar: any
     //객체 테스트
 };
 
 export const ActionSection = (props: Props) => {
     return (
         <div>
-            <SelectAction 
-            action={props.action} 
-            setAction={props.setAction} />
-            <SendButton 
-            data={props.data}
-            cohort = {props.cohort}
-            nth = {props.nth}
-            gitCheck = {props.gitCheck}
-            slackCheck = {props.slackCheck}
-            calendar = {props.calendar}
+            <SelectAction
+                action={props.action}
+                setAction={props.setAction} />
+            <SendButton
+                data={props.data}
+                cohort={props.cohort}
+                nth={props.nth}
+                gitCheck={props.gitCheck}
+                slackCheck={props.slackCheck}
+                calendar={props.calendar}
             />
         </div>
     );
